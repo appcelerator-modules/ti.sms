275ccbf228bbdf22da6365bc2fe3d097cf464f26
hansemannn <hans.knoechel@hs-osnabrueck.de>
2019-10-06 09:44:16 +0200

    fix: add back timodule.xml

--- ios/Classes/TiSmsModuleAssets.m
+++ ios/Classes/TiSmsModuleAssets.m
@@ -3,13 +3,22 @@
  */
 #import "TiSmsModuleAssets.h"
 
-extern NSData * dataWithHexString (NSString * hexString);
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
 
 @implementation TiSmsModuleAssets
 
-- (NSData*) moduleAsset
+- (NSData *)moduleAsset
 {
-	return nil;
+  
+
+  return nil;
+}
+
+- (NSData *)resolveModuleAsset:(NSString *)path
+{
+  
+
+  return nil;
 }
 
 @end
